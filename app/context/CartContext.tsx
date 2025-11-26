d371fb5ad22eee6a195e458a24c647c6f77268be
Ritik kumar <ritikkumarkashyap9@gmail.com>
2025-11-26 12:44:49 +0530

    persists cart

--- app/context/CartContext.tsx
+++ app/context/CartContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { CartContextType } from "@/app/types/CartContextType";
 import { CartProductType } from "@/app/types/CartProductTypes";
 
@@ -10,9 +10,23 @@ export const CartContextProvider = ({ children }: { children: React.ReactNode })
   const [cartTotalQty, setCartTotalQty] = useState(10);
   const [cartProducts, setCartProducts] = useState<CartProductType[]>([]);
 
-  const handleAddProductToCart = useCallback((product: CartProductType) => {
-    setCartProducts((prev) => [...prev, product]);
-  }, []);
+
+
+
+ const handleAddProductToCart = useCallback((product: CartProductType) => {
+ setCartProducts((prev) => {
+  const updatedCart = [...(prev ?? []), product];
+  localStorage.setItem("eShopCartItems", JSON.stringify(updatedCart));
+  return updatedCart;
+});
+
+}, []);
+
+  useEffect(() => {
+    const cartItems: any = localStorage.getItem("eShopCartItems")
+    const cProducts: CartProductType[] = JSON.parse(cartItems);
+    setCartProducts(cProducts);
+  },[])
 
   const value: CartContextType = {
     cartTotalQty,
